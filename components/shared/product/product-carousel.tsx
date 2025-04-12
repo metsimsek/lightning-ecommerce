c24116622b79ee703acee84e9050478ed212aa72
Metin Simsek <metin.simsek@sabstt.com>
2025-04-12 11:47:08 +0100

    change product carousel view

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -30,7 +30,7 @@ const ProductCarousel = ({ data }: { data: Product[] }) => {
       <CarouselContent>
         {data.map((product: Product) => (
           <CarouselItem key={product.id}>
-            <Link href={`/product/${product.slug}`}>
+            <Link href={`/search`}>
               <div className='relative mx-auto'>
                 <Image
                   src={product.banner!}
@@ -40,11 +40,6 @@ const ProductCarousel = ({ data }: { data: Product[] }) => {
                   sizes='100vw'
                   className='w-full h-auto'
                 />
-                <div className='absolute inset-0 flex items-end justify-center'>
-                  <h2 className='bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white'>
-                    {product.name}
-                  </h2>
-                </div>
               </div>
             </Link>
           </CarouselItem>
